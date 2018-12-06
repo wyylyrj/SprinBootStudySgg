4f4e5875ef6a0a3c085f6e80ab5b5a7e051abedf
wyylyrj <461318558@qq.com>
2018-12-07 01:18:24 +0800

    SpringBootStudySgg

--- src/main/java/com/yrj/config/MyMvcConfig.java
+++ src/main/java/com/yrj/config/MyMvcConfig.java
@@ -1,24 +1,32 @@
 package com.yrj.config;
 
+import com.yrj.component.LoginHandlerInterceptor;
 import com.yrj.component.MylocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
-        @Override
-        public void addViewControllers(ViewControllerRegistry registry) {
-            registry.addViewController("/").setViewName("login");
-            registry.addViewController("/index.html").setViewName("login");
-            registry.addViewController("/main.html").setViewName("dashboard");
-        }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+        registry.addViewController("/main.html").setViewName("dashboard");
+    }
 
-        @Bean
-        public LocaleResolver localeResolver(){
-            return new MylocaleResolver();
-        }
+    @Bean
+    public LocaleResolver localeResolver() {
 
+        return new MylocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/","/index.html","/user/login","/static/**","/webjars/**");
+    }
 }
---
+++ src/main/java/com/yrj/contorller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.yrj.contorller;
+
+import com.yrj.dao.EmployeeDao;
+import com.yrj.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+    @GetMapping("emps")
+    public String list(Model model){
+        Collection<Employee> all = employeeDao.getAll();
+        model.addAttribute("emps",all);
+        return "emp/list";
+    }
+}
