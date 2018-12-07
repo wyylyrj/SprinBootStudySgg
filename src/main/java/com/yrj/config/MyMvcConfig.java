671e8dea020802dc21a6ca63c366c6f509dc9603
wyylyrj <461318558@qq.com>
2018-12-07 15:54:47 +0800

    SpringBootStudySgg

--- src/main/java/com/yrj/config/MyMvcConfig.java
+++ src/main/java/com/yrj/config/MyMvcConfig.java
@@ -28,7 +28,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/","/index.html","/user/login","/asserts/**","/webjars/**");
+                .excludePathPatterns("/","/index.html","/user/login","/static/**","/webjars/**");
     }
 
     @Override
--- src/main/java/com/yrj/contorller/EmployeeController.java
+++ src/main/java/com/yrj/contorller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.yrj.contorller;
 
+import com.yrj.dao.DepartmentDao;
 import com.yrj.dao.EmployeeDao;
+import com.yrj.entities.Department;
 import com.yrj.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -13,10 +15,48 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
     @GetMapping("emps")
     public String list(Model model){
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("emps",all);
         return "emp/list";
+    }
+
+    @GetMapping("emp")
+    public String toAddPage(Model model){
+        Collection<Department> depts = departmentDao.getDepartments();
+        model.addAttribute("depts",depts);
+        return "emp/add";
+    }
+
+    @PostMapping("emp")
+    public String addEmp(Employee employee){
+        System.out.println("保存的员工信息："+employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        Collection<Department> depts = departmentDao.getDepartments();
+        model.addAttribute("depts",depts);
+        return "/emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee){
+        System.out.println("修改的员工信息：" + employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
