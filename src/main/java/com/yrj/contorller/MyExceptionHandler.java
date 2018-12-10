89e22d3d84da541e86501a85875a9816d48c457e
wyylyrj <461318558@qq.com>
2018-12-10 23:20:48 +0800

    SpringBootStudySgg

--- src/main/java/com/yrj/contorller/MyExceptionHandler.java
+++ src/main/java/com/yrj/contorller/MyExceptionHandler.java
@@ -19,12 +19,14 @@ public class MyExceptionHandler {
 //        map.put("message",e.getMessage());
 //        return map;
 //    }
+    //@ResponseBody
     @ExceptionHandler(UserNotExistException.class)
     public String handlerException(Exception e, HttpServletRequest request){
         Map<String,Object> map = new HashMap<>();
-        request.setAttribute("javax.servlet.error.status_code",400);
+        request.setAttribute("javax.servlet.error.status_code",500);
         map.put("code","user.notexist");
         map.put("message",e.getMessage());
         return "forward:/error";
+        //return map;
     }
 }
